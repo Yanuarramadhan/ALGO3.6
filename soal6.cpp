117beb6431203aa9f82413d73d372a9cfc2780a0
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-13 16:22:23 +0700

    Add files via upload

---
+++ soal6.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+
+    float v,s,t;
+    printf("masukan kecepatanya=");
+    scanf ("%f",&v);
+    printf(" masukan waktu yang di tempuh");
+    scanf ("%f",&t);
+    s=v*t;
+    printf("maka jarak yang ditempuh adalah=%f",s);
+    return 0;}
